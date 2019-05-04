de520b092b71d60d1266a8b89b7b71201e1195b2
Ekscursionist <larakek@gmail.com>
2019-05-04 11:40:17 -0400

    adding some buttons; little bit more CSS; starting to work on basic JS functionality

--- js.js
+++ js.js
@@ -1,10 +1,54 @@
-//name generator
+$(document).ready(function() {
+  //name generator
 
-//race/class generator
+  //race/class generator
 
-var racesarray = [];
-var classesarray = [];
+  var racesarray = [
+    "Elf",
+    "Orc",
+    "Human",
+    "Halfling",
+    "Dwarf",
+    "Gnome",
+    "Half-Elf",
+    "Half-Orc"
+  ];
+  var classesarray = [
+    "Rogue",
+    "Bard",
+    "Cleric",
+    "Paladin",
+    "Barbarian",
+    "Ranger",
+    "Wizard"
+  ];
 
-//stats generator
+  //list the races in the racelistspan
+  var racesspan = $("#racelistspan");
+  for (var i = 0; i < racesarray.length; i++) {
+    console.log(i);
+    var listraceswithcommas = racesarray[i] + ", ";
+    racesspan.append(listraceswithcommas);
+  }
 
-var statsarray = ["STR", "WIS", "INT", "DEX", "CHA", "CON"];
+  //list the classes in the classlistspan
+  var classesspan = $("#classlistspan");
+  for (var i = 0; i < classesarray.length; i++) {
+    console.log(i);
+    var listclasseswithcommas = classesarray[i] + ", ";
+    classesspan.append(listclasseswithcommas);
+  }
+
+  //stats generator
+
+  var statsarray = ["STR", "WIS", "INT", "DEX", "CHA", "CON"];
+
+  // -------------------------------------------------------
+  //standin for while I'm WIPing
+  $("#newcharbtn").click(function() {
+    alert(
+      "you sure clicked that button.  but the app isn't ready.  please wait some more."
+    );
+    //note: this button will eventually create a new everything
+  });
+});
