6913c3691e98324aaa97cc42dfc02fb3a93446c7
Ekscursionist <larakek@gmail.com>
2019-05-16 19:30:02 -0400

    continuing tabs, adding colors, experimenting with colors, and testing out formats

--- dndchargen/src/Components/MainContainer/MainContainer.jsx
+++ dndchargen/src/Components/MainContainer/MainContainer.jsx
@@ -14,33 +14,58 @@ class MainContainer extends React.Component {
           <Row id="boop">
             <Col sm={3}>
               <Nav variant="tabs" className="flex-column">
-                <Nav.Item className="eachTab">
+                <Nav.Item className="eachTab" id="tabone">
                   <Nav.Link eventKey="first">Tab 1</Nav.Link>
                 </Nav.Item>
-                <Nav.Item className="eachTab">
+                <Nav.Item className="eachTab" id="tabtwo">
                   <Nav.Link eventKey="second">Tab 2</Nav.Link>
                 </Nav.Item>
-                <Nav.Item className="eachTab">
+                <Nav.Item className="eachTab" id="tabthree">
                   <Nav.Link eventKey="third">Tab 3</Nav.Link>
                 </Nav.Item>
-                <Nav.Item className="eachTab">
+                <Nav.Item className="eachTab" id="tabfour">
                   <Nav.Link eventKey="fourth">Tab 4</Nav.Link>
                 </Nav.Item>
-                <Nav.Item className="eachTab">
+                <Nav.Item className="eachTab" id="tabfive">
                   <Nav.Link eventKey="fifth">Tab 5</Nav.Link>
                 </Nav.Item>
               </Nav>
             </Col>
-            <Col sm={9}>
+            <Col sm={9} id="justhiscol">
               <Tab.Content>
                 <Tab.Pane eventKey="first">
-                  <p>test tab pane 1</p>
+                  <h1>first tab</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Etiam ornare commodo dapibus. Fusce elementum diam aliquet
+                    mi elementum lacinia. Proin at lobortis dolor. Fusce quis
+                    turpis risus. Pellentesque a mi ac magna maximus commodo ac
+                    eget urna. Aliquam nibh orci, elementum eu pulvinar eget,
+                    iaculis a felis. Mauris ex turpis, pulvinar eget diam a,
+                    bibendum porta lectus. Nam non tortor purus. Suspendisse
+                    potenti.
+                  </p>
+                  <p>123456790</p>
+                  <p>ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
+                  <p>abcdefghijklmnopqrstuvwxyz</p>
                 </Tab.Pane>
                 <Tab.Pane eventKey="second">
-                  <p>test tab pane 2</p>
+                  <h1>two'th tab</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                  </p>
+                  <p>123456790</p>
+                  <p>ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
+                  <p>abcdefghijklmnopqrstuvwxyz</p>
                 </Tab.Pane>
                 <Tab.Pane eventKey="third">
-                  <p>test tab pane 3</p>
+                  <h1>TAB 3</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                  </p>
+                  <p>123456790</p>
+                  <p>ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
+                  <p>abcdefghijklmnopqrstuvwxyz</p>
                 </Tab.Pane>
                 <Tab.Pane eventKey="fourth">
                   <p>test tab pane 4</p>
